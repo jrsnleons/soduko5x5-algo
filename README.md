0b0c10f4c0a9e442fa03f4f2420491d9989646c3
jello_ <67309482+jrsnleons@users.noreply.github.com>
2023-10-18 18:02:00 +0800

    add given to table function

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Soduko 5x5 Algorithm
-An algorithm that solves a 5x5 soduko table that a user inputs with 5 numbers from 1 to 5. 
+An algorithm that solves a 5x5 soduko table that a user inputs with 5 numbers from 1 to 5.
+
+## To do 
+[ ] input validation
+[ ] brute force algorithm
+[ ]
--- main.py
+++ main.py
@@ -1,12 +1,39 @@
+import numpy as np
+
+array = [[0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0,]]
+
+def output_table(array):
+    print("    1 2 3 4 5")
+    print("   -----------")
+    for count, r in enumerate(array):
+        print(count + 1 , end = " | ")
+        for c in r:
+            print(c, end = " ")
+        print() 
+
+def addnum(col, row, array, num):
+    array[col-1][row-1] = num
+
+def init_given_table(array):
+    n = 1
+    while n <= 5:
+        r = 0
+        c = 0
+        x, y = input("Enter the coordinates where you want to add " + str(n) + " [x y]: ").split()
+        x = int(x)
+        y = int(y)
+        if(x < 6 and x > 0 and y < 6 and y > 0) :
+            addnum(x, y, array, n)
+            output_table(array)
+            n+=1
+        else:
+            print("Please only input valid numbers (1-5)")
 
 
-def init_table():
-    print("Hello world")
 
-
-
-def main():
-    print ("Pokemon")
-    init_table()
-    
-main()
+output_table(array)
+init_given_table(array)
